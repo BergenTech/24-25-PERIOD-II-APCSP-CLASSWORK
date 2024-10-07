bfd8b93e4770d8d459ce683e94119c904f4a7b92
emrgem <emrgem@bergen.org>
2024-10-07 09:26:49 -0400

    bulls eye

--- Unit 3 - Intro to P5.JS/Lesson8-loops/1-while-loops/bullseye.js
+++ Unit 3 - Intro to P5.JS/Lesson8-loops/1-while-loops/bullseye.js
@@ -1,8 +1,26 @@
+let thickness = 15, gap = 50
+let randomColor
 function setup(){
-    createCanvas(400,400)
+    createCanvas(600,600)
+    ellipseMode(RADIUS)
+    frameRate(10)
+    randomColor = color(random(256),random(256),random(256))
 }
 
 function draw(){
     background(220)
+    let radius = 300
+    // translate(width/2, height/2)
+    // ellipse(0,0,radius)
+    // translate(100,50)
+    // ellipse(0,0,radius)
+    while(radius>=50){
+        strokeWeight(thickness)
+        // stroke('red')
+        stroke(randomColor)
+        randomColor = color(random(256),random(256),random(256))
+        ellipse(width/2,height/2,radius)
+        radius = radius - gap
+    }
 
 }
